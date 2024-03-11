8e4691f21c8e514936c24eb1322bf24105646f6c
Aakash <aakash2506c@gmail.com>
2024-03-11 13:49:19 +0530

    ok

--- 03_PURETS/dist/index.js
+++ 03_PURETS/dist/index.js
@@ -3,7 +3,17 @@ class User {
     constructor(name, email, country) {
         this.name = name;
         this.email = email;
-        this.country = "";
+        this.country = country;
+        this.movie_count = 1;
+    }
+    get getName() {
+        return `${this.email}`;
+    }
+    set movieCount(movie_count) {
+        if (movie_count < 1) {
+            throw new Error("Actor should have atleast one movie");
+        }
+        this.movie_count = movie_count;
     }
 }
 const bestActor2025 = new User("Aakash", "aakash2506c");
--- 03_PURETS/src/index.ts
+++ 03_PURETS/src/index.ts
@@ -1,8 +1,20 @@
 class User{
-
-    private readonly country?:string=""
-  constructor(public name:string,public email:string,country?:string){
+   private movie_count:number=1;
+    
+  constructor(public name:string,public email:string,private readonly country?:string){
   }
+
+   get getName():string{
+      return `${this.email}`
+   }
+
+   set movieCount(movie_count:number) {
+            if(movie_count<1){
+                throw new Error("Actor should have atleast one movie")
+            }
+               this.movie_count=movie_count;
+
+   }
 }
 
 const bestActor2025=new User("Aakash","aakash2506c")
